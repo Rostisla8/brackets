cb0015bd2a5c5d2bec01b97d1cf5db2a8f26f150
Rostisla8 <86406897+Rostisla8@users.noreply.github.com>
2022-02-25 09:40:53 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const array = [];
+  const configArray = bracketsConfig.flat();
+
+  for (let i = 0; i < str.length; i++) {
+    const symb = str[i];
+    const index = configArray.indexOf(symb);
+    const stackLastIndex = array.length - 1;
+    const topElement = array[stackLastIndex];
+
+    if (index % 2 === 0) {
+      if (symb === configArray[index + 1]  && topElement === symb) {
+        array.pop();
+      } 
+      else {
+        array.push(symb);
+      }
+  
+    } else {
+      if (!array.length) {
+        return false;
+      }
+      if (topElement === configArray[index - 1]) {
+        array.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return !array.length;
+};
